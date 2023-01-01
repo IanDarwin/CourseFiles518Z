233a45cb27c11b0986dbaec99fed7f1be5782581
Ian Darwin <ian@darwinsys.com>
2023-01-01 16:58:04 -0500

    Average.dart - simple program to get started

---
+++ sourcecode/donows/lib/average.dart
@@ -0,0 +1,15 @@
+void main() {
+
+	double grade1 = 76.0;
+	double grade2 = 83.0;
+	double grade3 = 62.0;
+
+
+	//  TODO:
+	//
+	//  a.  Compute and display the average grade
+
+	//  b.  Display a PASS/FAIL message
+	//        average >= 75.0  PASS
+	//        average <  75.0  FAIL
+}
