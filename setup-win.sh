27024cc7eeed09fd425ffb0225edab40763c17b0
Ian Darwin <ian@darwinsys.com>
2023-01-14 09:08:34 -0600

    Get more working setup-win

--- setup-win.sh
+++ setup-win.sh
@@ -3,7 +3,14 @@
 echo Androd Module: Installs Part One 
 echo You must have installed git:
 echo winget install -i Git.Git 
-echo And chosen the "Unix utilities path"!
+echo And chosen the "'Unix utilities path'"!
+echo
+
+# Set up some version numbers
+INTELLIJ_VER=2022.3.1
+AS_VER=2021.3.1.17
+FLUTTER_VER=3.3.10-stable
+MAVEN_VER=3.8.7
 
 # Make sure we start in ~
 cd $HOME
@@ -12,10 +19,12 @@ mkdir bin lib
 
 set -e # from here, bomb if error
 
-if [ false ]; then
+if false; then
 
 echo Windows Terminal - tabbed terminal with CTRL/C-CTRL-V
 # winget install --id Microsoft.WindowsTerminal
+echo "Had to manually install via store??"
+
 echo Java JDK 17 LTS - free from numerous OpenJDK download sites
 echo done  winget install --id EclipseAdoptium.Temurin.17.JDK --version 17.0.4.8
 PATH="/c/Program Files/Eclipse Adoptium/jdk-17.0.4.8-hotspot/bin:$PATH"
@@ -44,7 +53,6 @@ expenses-server,\
 fluttersrc,\
 flowcase,\
 jmemory,\
-l10n_counter_way1,\
 makehandsons\
 } \
 	https://github.com/flutter/samples \
@@ -58,16 +66,16 @@ do
 	fi
 done
 
-fi # ================== END ================
-
 echo Tools used in preparing the course load
-echo Make (gnu-make) - 
+echo "Make (gnu-make)"
 winget install --id GnuWin32.Make
 echo nmap just for ncat
 winget install --id Insecure.Nmap
 
+fi # ================== END OF SKIP ================
+
 echo Downloading maven build tool from https://maven.apache.org/download
-curl https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz | tar xzf -
+curl https://dlcdn.apache.org/maven/maven-3/${MAVEN_VER}/binaries/apache-maven-${MAVEN_VER}-bin.tar.gz | tar xzf -
 
 echo Documentation
 curl -o Documents/LevinInternalsBookVol1FirstEdn.pdf http://newandroidbook.com/AIvI-M-RL1.pdf
@@ -77,25 +85,21 @@ CALL ..\apache-maven-3.8.6\bin\mvn.cmd -ntp -DskipTests clean package install as
 copy target\makehandsons-1.0-SNAPSHOT-jar-with-dependencies.jar $HOME\lib\makehandsons.jar
 copy scripts\* $HOME\bin
 cd ..
-git clone https://github.com/IanDarwin/CourseFiles510G
+
 echo Generating exercise files from solutions
-cd CourseFiles510G\sourcecode
+cd CourseFiles518Z\sourcecode
 make
 cd ..\..
 
 echo Chapter 3 Expenses-server - from course author, to upload expenses for Expenses app  
-git clone https://github.com/IanDarwin/expenses-server
-echo Pre-fetch a bunch ("half the internet") of dependencies for Spring-boot server
+echo 'Pre-fetch a bunch ("half the internet") of dependencies for Spring-boot server'
 CALL apache-maven-3.8.6\bin\mvn.cmd -ntp -f expenses-server/pom.xml compile
 
-echo Grand finale: download a tranche of files in non-git format
-curl https://darwinsys.com/tmp/Tilde510G.tgz | tar xzf -
-
-echo Java Version - default should be 17, not 11
-java --version
+# echo Grand finale: download a tranche of files in non-git format
+# curl https://darwinsys.com/tmp/Tilde518Z.tgz | tar xzf -
 
 @echo Tha-Tha-Tha-That's all folks!
 
 @echo echoember there is one manual step needed:
 @echo Set All Browser Home Pages to 
-@echo	file:///C:/Users/%USERNAME%/CourseFiles510G/website/index.html
+@echo	file:///C:/Users/%USERNAME%/CourseFiles518Z/website/index.html
