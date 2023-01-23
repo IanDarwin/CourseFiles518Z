4c147eb0d92e8158d380da3067aeff508cf19be4
Ian Darwin <ian@darwinsys.com>
2023-01-23 15:48:28 -0500

    Add picture-taking code. Works on Pixel 6A.
    Fails sadly on emulator (API31, X64), maybe others.

--- ex51solution/lib/main.dart
+++ ex51solution/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/foundation.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 void main() {
   runApp(const AnotherDemoApp());
@@ -30,7 +31,8 @@ class DemoHomePage extends StatefulWidget {
 }
 
 class _DemoHomePageState extends State<DemoHomePage> {
-
+  XFile? imageFile;
+  CameraController? cameraController;
   @override
   Widget build(BuildContext context) {
     debugPrint("In _DemoHomePageState.build()");
@@ -50,12 +52,20 @@ class _DemoHomePageState extends State<DemoHomePage> {
                   ),
                   //const Spacer(flex: 30),
                   ElevatedButton(
-                    onPressed: () => debugPrint("Camera Button pressed"),
+                    onPressed: () => _doTakePicture(),
                     child: const Text('Take Picture'),
                   ),
                 ],
               ),
-              // XXX Need Image here
+              imageFile == null ?
+              const Text("No picture yet") :
+              // Constrain the size so we don't overflow:
+              Container(
+                width: 200,
+                height: 300,
+                alignment: Alignment.center,
+                child: Image.file(File(imageFile!.path)),
+              ),
             ]),
         floatingActionButton: FloatingActionButton(
           onPressed: () => debugPrint("FAB pressed"),
@@ -63,5 +73,51 @@ class _DemoHomePageState extends State<DemoHomePage> {
           child: const Icon(Icons.add),
         )
     );
+  }
+
+  void _doTakePicture() async  {
+    debugPrint("in _doTakePicture()");
+    late CameraDescription camera;
+    var cameras = await availableCameras();
+    if (cameras.isNotEmpty) {
+      camera = cameras.first;
+    }
+
+    cameraController =
+        CameraController(camera, ResolutionPreset.medium);
+    await cameraController!.initialize();
+    if (!cameraController!.value.isInitialized) {
+      debugPrint('Error: select a camera first.');
+      return null;
+    }
+
+    if (cameraController!.value.isTakingPicture) {
+      // Capture already in the works, give up
+      return null;
+    }
+
+    late final XFile file;
+    try {
+      debugPrint("About to takePicture()");
+      file = await cameraController!.takePicture();
+      debugPrint("Image is in file $file");
+    } on CameraException catch (e) {
+      debugPrint("NO PICTURE Error: $e");
+      return null;
+    }
+    if (mounted) {
+      setState(() {
+        debugPrint("Image is in file $file");
+        imageFile = file;
+      });
+      debugPrint('Picture saved to ${file.path}');
+    }
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    cameraController!.dispose();
+    super.dispose();
   }
 }
