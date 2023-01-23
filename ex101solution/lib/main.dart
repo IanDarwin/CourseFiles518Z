496a9a30e2066116e463cb55e6701ecf952629a7
Ian Darwin <ian@darwinsys.com>
2023-01-22 19:53:08 -0500

    Reconcile ex101{,solution}/main.dart, l10n files

--- ex101solution/lib/main.dart
+++ ex101solution/lib/main.dart
@@ -45,121 +45,123 @@ class _TableDemoState extends State<TableDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+        appBar: AppBar(title: Text(widget.title)),
         body: ListView(
-        children: [
-          const Text('Basic Table'),
-          Container(
-            height: 200,
-            padding: const EdgeInsets.all(7),
-            child:Table(
-                border: TableBorder.all(),
-                columnWidths: const <int, TableColumnWidth>{
-                  0: FixedColumnWidth(128),
-                  1: FlexColumnWidth(),
-                  2: FixedColumnWidth(128),
-                },
-                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                children: <TableRow>[
-                  TableRow(
-                    children: <Widget>[
-                      TableCell(
-                        verticalAlignment: TableCellVerticalAlignment.top,
-                        child: Container(
-                          height: 64,
-                          width: 64,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Container(
-                        height: 32,
-                        width: 64,
-                        color: Colors.green,
-                      ),
-                      Container(
-                        height: 64,
-                        color: Colors.blue,
-                      ),
-                    ],
-                  ),
-                  const TableRow(children: [
-                    SizedBox(height: 64, child: Text("R2C1")),
-                    TableCell(
-                        verticalAlignment: TableCellVerticalAlignment.middle,
-                        child: Center(child: Text("R2C2"))),
-                    TableCell(
-                        verticalAlignment: TableCellVerticalAlignment.bottom,
-                        child: Text("R2C3", textAlign: TextAlign.end)),
-                  ])
-                ]
-            ),
-          ),
+          children: [
 
-          // SECOND TABLE
-          Text(S.of(context).tableTitle),
-          Container(
-            padding: const EdgeInsets.all(7),
-            child: Table(
-                border: TableBorder.all(),
-                columnWidths: const <int, TableColumnWidth>{
-                  0: FixedColumnWidth(80),
-                  1: FlexColumnWidth(),
-                  2: FixedColumnWidth(80),
-                },
-                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                children: <TableRow>[
-                  TableRow(
-                      children: <Widget>[
-                        Text(S.of(context).date, style: const TextStyle(fontWeight: FontWeight.bold)),
-                        Text(S.of(context).item, style: const TextStyle(fontWeight: FontWeight.bold)),
-                        Text(S.of(context).amount, style: const TextStyle(fontWeight: FontWeight.bold)),
-                        Text(S.of(context).balance, style: const TextStyle(fontWeight: FontWeight.bold)),
-                      ]
-                  ),
-                  const TableRow(
-                      children: <Widget>[
-                        Text("2023-02-15"),
-                        Text("Deposit ATM#42"),
-                        Text("123.00", textAlign: TextAlign.end),
-                        Text("456.00", textAlign: TextAlign.end),
-                      ]
-                  ),
-                  const TableRow(
-                      children: <Widget>[
-                        Text("2023-02-20"),
-                        Text("Withdrawal ATM#42"),
-                        Text("15.00", textAlign: TextAlign.end),
-                        Text("441.00", textAlign: TextAlign.end),
-                      ]
-                  ),
-                ]
-            ),
-          ),
-
-          // THIRD TABLE
-          Text(S.of(context).dataTable),
-          Container(
+            // FIRST TABLE
+            Text(S.of(context).basicTable),
+            Container(
+              height: 200,
               padding: const EdgeInsets.all(7),
-              child: DataTable(
-                border: TableBorder.all(),
-                columns: [
-                  DataColumn(label:(Text(S.of(context).date))),
-                  DataColumn(label:(Text(S.of(context).item))),
-                  DataColumn(label:(Text(S.of(context).amount))),
-                  DataColumn(label:(Text(S.of(context).balance))),
-                ],
-                rows: const [
-                  DataRow(cells: [DataCell(Text('2022-02-15')),
-                    DataCell(Text('Deposit ATM#42')),
-                    DataCell(Text("123.00")),
-                    DataCell(Text("456.00"))
+              child:Table(
+                  border: TableBorder.all(),
+                  columnWidths: const <int, TableColumnWidth>{
+                    0: FixedColumnWidth(128),
+                    1: FlexColumnWidth(),
+                    2: FixedColumnWidth(128),
+                  },
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  children: <TableRow>[
+                    TableRow(
+                      children: <Widget>[
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.top,
+                          child: Container(
+                            height: 64,
+                            width: 64,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Container(
+                          height: 32,
+                          width: 64,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          height: 64,
+                          color: Colors.blue,
+                        ),
+                      ],
+                    ),
+                    const TableRow(children: [
+                      SizedBox(height: 64, child: Text("R2C1")),
+                      TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Center(child: Text("R2C2"))),
+                      TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.bottom,
+                          child: Text("R2C3", textAlign: TextAlign.end)),
+                    ])
+                  ]
+              ),
+            ),
+
+            // SECOND TABLE
+            Text(S.of(context).tableTitle),
+            Container(
+              padding: const EdgeInsets.all(7),
+              child: Table(
+                  border: TableBorder.all(),
+                  columnWidths: const <int, TableColumnWidth>{
+                    0: FixedColumnWidth(80),
+                    1: FlexColumnWidth(),
+                    2: FixedColumnWidth(80),
+                  },
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  children: <TableRow>[
+                    TableRow(
+                        children: <Widget>[
+                          Text(S.of(context).date, style: const TextStyle(fontWeight: FontWeight.bold)),
+                          Text(S.of(context).item, style: const TextStyle(fontWeight: FontWeight.bold)),
+                          Text(S.of(context).amount, style: const TextStyle(fontWeight: FontWeight.bold)),
+                          Text(S.of(context).balance, style: const TextStyle(fontWeight: FontWeight.bold)),
+                        ]
+                    ),
+                    const TableRow(
+                        children: <Widget>[
+                          Text("2023-02-15"),
+                          Text("Deposit ATM#42"),
+                          Text("123.00", textAlign: TextAlign.end),
+                          Text("456.00", textAlign: TextAlign.end),
+                        ]
+                    ),
+                    const TableRow(
+                        children: <Widget>[
+                          Text("2023-02-20"),
+                          Text("Withdrawal ATM#42"),
+                          Text("15.00", textAlign: TextAlign.end),
+                          Text("441.00", textAlign: TextAlign.end),
+                        ]
+                    ),
+                  ]
+              ),
+            ),
+
+            // THIRD TABLE
+            Text(S.of(context).dataTable),
+            Container(
+                padding: const EdgeInsets.all(7),
+                child: DataTable(
+                  border: TableBorder.all(),
+                  columns: [
+                    DataColumn(label:(Text(S.of(context).date))),
+                    DataColumn(label:(Text(S.of(context).item))),
+                    DataColumn(label:(Text(S.of(context).amount))),
+                    DataColumn(label:(Text(S.of(context).balance))),
                   ],
-                  )
-                ],
-              )
-          )
-        ],
+                  rows: const [
+                    DataRow(cells: [DataCell(Text('2022-02-15')),
+                      DataCell(Text('Deposit ATM#42')),
+                      DataCell(Text("123.00")),
+                      DataCell(Text("456.00"))
+                    ],
+                    )
+                  ],
                 )
+            )
+          ],
+        )
     );
   }
 }
